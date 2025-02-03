1a221a318b8cf13b5772cb20703a359b5ddf7399
HannahPlum <ha537956@ucf.edu.com>
2025-02-03 16:21:44 -0500

    completed to do tasks 1-4 as required by the React Application Assignment

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,45 +1,68 @@
-import { useState, useCallback } from "react"
+import { useState, useCallback } from "react";
 /**
  * Renders an array of strings passed in that can be filtered and added to as an
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
-  let [newMenuItem, setNewMenuItem] = useState("")
+export default function Sidebar({ initialMenuItems }) {
+  let [newMenuItem, setNewMenuItem] = useState("");
+
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
-  let [filter, setFilter] = useState("")
+
+  let [menuItems, setMenuItems] = useState(initialMenuItems);
+  let [filter, setFilter] = useState("");
+
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
+
   let addMenuItem = useCallback(() => {
-    console.log("Added menu item")
+    console.log("Added menu item");
+
     //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
-    //   // This involves adding a parameter and changing a class instance variable (props).
-    //   setMenuItems([item, ...menuItems])
-  }, [])
+    //This involves adding a parameter and changing a class instance variable (props).
+
+    setMenuItems([newMenuItem, ...menuItems]);
+    setNewMenuItem("");
+  }, [newMenuItem, menuItems]);
 
   // TODO: 4. Display ONLY the menu items that contain the filter element value
-  // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
+  // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.4
+
+  //filtering using regex, i ensures the value being filtered is case insensitive.
+
+  const filteredMenuItems = menuItems.filter((item) => {
+    const regex = new RegExp(filter, "i");
+    return regex.test(item);
+  });
 
   // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
   // its own item.
+
   return (
     <div>
+      <ul>
+        {filteredMenuItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
       <input
         type="text"
         id="newMenuItemValue"
         value={newMenuItem}
         onChange={(event) => setNewMenuItem(event.target.value)}
       ></input>
+
       <br />
       <button
         onClick={() => {
-          /* TODO: 3 */
+          addMenuItem();
         }}
       >
         Add Item
       </button>
       <br />
+
       <input
         id="filter"
         type="text"
@@ -48,5 +71,5 @@ export default function Sidebar() {
         placeholder="Filter by..."
       ></input>
     </div>
-  )
+  );
 }
